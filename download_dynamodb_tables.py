1779ca0b45c7384c5cc56524812acb98b6f301c4
anandshivam44 <anand.shivam44@yahoo.com>
2021-08-06 01:05:40 +0530

    test 9

--- download_dynamodb_tables.py
+++ download_dynamodb_tables.py
@@ -1,6 +1,7 @@
 import boto3
 import json
 import os
+from decimal import Decimal
 
 tables = []
 
@@ -11,18 +12,15 @@ for i in range(10):  # BACKUP_TABLE_NAME_1
     TABLE_NAME = "BACKUP_TABLE_NAME_" + str(i)
     if os.environ[TABLE_NAME] != "NULL":
         tables.append(os.environ[TABLE_NAME])
+# tables.append("Movies")
 
 REGION = os.getenv("REGION", "us-east-1")
 
 
-class DecimalEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, decimal.Decimal):
-            # wanted a simple yield str(o) in the next line,
-            # but that would mean a yield on the line with super(...),
-            # which wouldn't work (see my comment below), so...
-            return (str(o) for o in [o])
-        return super(DecimalEncoder, self).default(o)
+def default(obj):
+    if isinstance(obj, Decimal):
+        return str(obj)
+    raise TypeError("Object of type '%s' is not JSON serializable" % type(obj).__name__)
 
 
 for table in tables:
@@ -31,6 +29,6 @@ for table in tables:
     dynamodb = boto3.resource("dynamodb", region_name=REGION)
     table = dynamodb.Table(table)
     response = table.scan()  # Download Dynamodb Table Contents
-    json_object = json.dumps(response["Items"], indent=4, cls=DecimalEncoder)
+    json_object = json.dumps(response["Items"], indent=4, default=default)
     file.write(json_object)
     file.close()
