06a634456f2c60d45d841039249170d95bea49a6
skp2021 <86594584+skp2021@users.noreply.github.com>
2021-09-27 16:52:36 +0530

    Update trigger

--- index.py
+++ index.py
@@ -4,4 +4,8 @@ import os
 def handler(event, context):
     client = boto3.client("codebuild")
     PROJECT_NAME = os.getenv("PROJECT_NAME", "TerraformStateBackup")
-    response = client.start_build(projectName=PROJECT_NAME)
+    #response = client.start_build(projectName=PROJECT_NAME)
+    response = codepipeline.start_pipeline_execution(
+        name=os.getenv("BACKUP_PIPELINE_NAME")
+    )
+    print(f"CodePipeline Response : {response}")
