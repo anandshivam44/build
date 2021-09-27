793fef5019ec0b6d365905dfd9f73776b31adc0d
Tejas sheth <tejas.sheth@vodafone.com>
2021-09-27 13:12:41 +0000

    Index file updated

--- index.py
+++ index.py
@@ -2,8 +2,9 @@ import boto3
 import os
 
 def handler(event, context):
-    client = boto3.client("codebuild")
+    #client = boto3.client("codebuild")
+    client = boto3.client("codepipeline")
     PROJECT_NAME = os.getenv("PROJECT_NAME", "TerraformStateBackup")
-   # response = client.start_build(projectName=PROJECT_NAME)
-    response = codepipeline.start_pipeline_execution(name=os.getenv("BACKUP_PIPELINE_NAME"))
+    #response = client.start_build(projectName=PROJECT_NAME)
+    response = client.start_pipeline_execution(name=os.getenv("BACKUP_PIPELINE_NAME"))
     print(f"CodePipeline Response : {response}")
