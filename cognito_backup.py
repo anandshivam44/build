a6867df3cd45fa71317a308569f1d35a07cb9693
Tejas sheth <tejas.sheth@vodafone.com>
2021-10-06 12:23:48 +0000

    Cognito backup done

--- cognito_backup.py
+++ cognito_backup.py
@@ -9,37 +9,36 @@ user_pool_id=os.environ["USER_POOL_ID"]
 cognito_data = []
 
 
-try:
-    print(user_pool_id)
-    group_names=[]
-    groups=client.list_groups(
-        UserPoolId=user_pool_id,
-    )["Groups"]
-    for group in groups:
-        group_names.append(group["GroupName"])
-    print(group_names)
-    
-    for group_name in group_names:
-        users=client.list_users_in_group(
-            UserPoolId=user_pool_id,
-            GroupName=group_name,
-            Limit=50
-        )["Users"]
-        #users = list_users_in_group(group_name)
-        params={"group_name":group_name,"users":users}
-        print(params)
-        cognito_data.append(params)
-    
-    full_path = './BackupFolder/cognito_backup/'    
-    file_name = "cognito_data.json"
-    completePath = os. path. join(full_path, file_name)
-    file = open(completePath, "w+")
-    json_object = json.dumps(cognito_data, indent=4, default=default)
-    file.write(json_object)
-    file.close()
 
-except Exception as e:
-    print(f"Internal Exception {e}!")
+print(user_pool_id)
+group_names=[]
+groups=client.list_groups(
+    UserPoolId=user_pool_id,
+)["Groups"]
+for group in groups:
+    group_names.append(group["GroupName"])
+print(group_names)
+
+for group_name in group_names:
+    users=client.list_users_in_group(
+        UserPoolId=user_pool_id,
+        GroupName=group_name,
+        Limit=50
+    )["Users"]
+    #users = list_users_in_group(group_name)
+    params={"group_name":group_name,"users":users}
+    print(params)
+    cognito_data.append(params)
+
+full_path = './BackupFolder/cognito_backup/'    
+file_name = "cognito_data.json"
+completePath = os. path. join(full_path, file_name)
+file = open(completePath, "w+")
+json_object = json.dumps(cognito_data, indent=4, default=default)
+file.write(json_object)
+file.close()
+
+
 
 def default(obj):
     if isinstance(obj, Decimal):
